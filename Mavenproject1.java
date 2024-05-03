7c3b5be7cbc246859be977dbfdd30b7ae4ede543
Brian1808 <alonsobtz2000@gmail.com>
2024-05-02 23:20:53 -0300

    ola

--- Mavenproject1.java
+++ Mavenproject1.java
@@ -5,7 +5,7 @@ public class Mavenproject1 {
 
     public static void main(String[] args) {
         System.out.println("Hello World!2de22");
-        System.out.println("-----------------");
+        System.out.println("---s--------------");
 
     }
 }
